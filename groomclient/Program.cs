c2e213fdb348afb8283c928dd9533d3d6be5b537
ShivamSingh226 <shivamsingh226@gmail.com>
2025-04-17 16:29:06 +0530

    Added gRoom Client

---
+++ groomclient/Program.cs
@@ -0,0 +1,11 @@
+﻿using Grpc.Net.Client;
+using gRoom.gRPC.Messages;
+
+using var channel=GrpcChannel.ForAddress("http://localhost:5038/");
+var Client=new Groom.GroomClient(channel);
+Console.Write("Enter room name to register: ");
+var roomName=Console.ReadLine();
+var resp=Client.RegisterToRoom(new RoomRegistrationRequest{RoomName=roomName});
+Console.WriteLine($"Room Id : {resp.RoomId}");
+
+Console.Read();

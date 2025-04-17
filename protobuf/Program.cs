bc8b7b5ed96536e3d6cea9ab04a234599c78f4b7
ShivamSingh226 <shivamsingh226@gmail.com>
2025-04-17 11:49:13 +0530

    Added gRPC Chatroom with NodeJS and C#

---
+++ protobuf/Program.cs
@@ -0,0 +1,23 @@
+﻿using Google.Protobuf;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Course.Protobuf.Test;
+
+Console.WriteLine("Welcome to Protobuf test!");
+var emp=new Employee();
+emp.FirstName="Shivam";
+emp.LastName="Singh";
+emp.IsRetired=false;
+var birthdate=new DateTime(1976,7,9);
+birthdate=DateTime.SpecifyKind(birthdate, DateTimeKind.Utc);
+emp.BirthDate=Timestamp.FromDateTime(birthdate);
+emp.MaritalStatus=Employee.Types.MaritalStatus.Married;
+emp.PreviousEmployers.Add("Microsoft");
+emp.PreviousEmployers.Add("HP");
+using(var output=File.Create("emp.dat")){
+    emp.WriteTo(output);
+}
+Employee empFromFile;
+using(var input=File.OpenRead("emp.dat")){
+    empFromFile=Employee.Parser.ParseFrom(input);
+}
+Console.WriteLine("Protobuf Test compiled :)");
